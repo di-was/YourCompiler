97a13e7b351447bab3e5007695148e91de4dc642
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-11 12:19:59 +1100

    Add project files.

---
+++ YourCompiler/Controllers/CompileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YourCompiler.Controllers
+{
+    [Route("api/compile")]
+    [ApiController]
+    public class CompileController : ControllerBase
+    {
+        [HttpPost("{language}")]
+        public IActionResult Compile(string language, [FromBody] string code)
+        {
+            return Ok($"{language} Compile endpoint is compiling {code}");
+
+        }
+    }
+}
---
+++ YourCompiler/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>     {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
