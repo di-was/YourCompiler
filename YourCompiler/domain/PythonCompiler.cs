1724973767e539daee64ff1f9e1af604ba398aa4
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-12 14:27:13 +1100

    Skeleton for python compiler

---
+++ YourCompiler/domain/PythonCompiler.cs
@@ -0,0 +1,10 @@
+﻿namespace YourCompiler.domain
+{
+    public class PythonCompiler : ICompiler
+    {
+        public CompilerResult Compile(string code) {
+            CompilerResult result = new CompilerResult("Hello World", null);
+            return result;
+        }
+    }
+}
