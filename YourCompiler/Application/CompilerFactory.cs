dd82c996e05b07ad28374a530d6ac64c64401b85
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-12 14:50:31 +1100

    implementation simple factory desing pattern

---
+++ YourCompiler/Application/CompilerFactory.cs
@@ -0,0 +1,17 @@
+﻿using YourCompiler.Domain;
+
+namespace YourCompiler.Application
+{
+    public class CompilerFactory
+    {
+        public static ICompiler CreateCompiler(string language)
+        {
+            return language.ToLower() switch
+            {
+                "python" => new PythonCompiler(),
+                "javascript" => new JavaScriptCompiler(),
+                _ => throw new NotSupportedException($"Language '{language}' is not supported.")
+            };
+        }
+    }
+}
