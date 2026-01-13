efa302d764337fe364db85dce05a2d6b2c4a2ba3
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-13 21:11:38 +1100

    compiler concrete classes

---
+++ YourCompiler/Application/JavaScriptCompiler.cs
@@ -0,0 +1,12 @@
+﻿using YourCompiler.Domain;
+
+namespace YourCompiler.Application
+{
+    public class JavaScriptCompiler : ICompiler
+    {
+        public CompilerResult Compile(string code) {
+            CompilerResult result = new CompilerResult("Hello World from JavaScript", null);
+            return result;
+        }
+    }
+}
---
+++ YourCompiler/Application/PythonCompiler.cs
@@ -0,0 +1,12 @@
+﻿using YourCompiler.Domain;
+
+namespace YourCompiler.Application
+{
+    public class PythonCompiler : ICompiler
+    {
+        public CompilerResult Compile(string code) {
+            CompilerResult result = new CompilerResult("Hello World", null);
+            return result;
+        }
+    }
+}
