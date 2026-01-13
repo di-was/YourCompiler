ba8c1cc7ed983a7b5bd92e416e08208d10441c68
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-13 13:54:33 +1100

    container service

---
+++ YourCompiler/Infrastructure/DockerService.cs
@@ -0,0 +1,62 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace YourCompiler.Infrastructure
+{
+    public class DockerService : IDockerService
+    {
+        public static async Task<string> runContainer(ContainerDetails details)
+        {
+            DockerClient client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient();
+
+            // Temporary folder
+            string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempDir);
+            string filePath = Path.Combine(tempDir, "main.py");
+            await System.IO.File.WriteAllTextAsync(filePath, details.code);
+
+            // Create container
+            var container = await client.Containers.CreateContainerAsync(new CreateContainerParameters
+            {
+                Image = "python:3.12",
+                Cmd = new List<string> { "python", "/app/main.py" },
+                AttachStdout = true,
+                AttachStderr = true,
+                HostConfig = new HostConfig
+                {
+                    AutoRemove = false,
+                    Binds = new List<string> { $"{tempDir}:/app" }
+                }
+            });
+
+            // Start container
+            await client.Containers.StartContainerAsync(container.ID, null);
+
+            // Wait for container to finish
+            var waitResponse = await client.Containers.WaitContainerAsync(container.ID);
+
+            // Get logs after it finished
+            var logs = await client.Containers.GetContainerLogsAsync(
+                container.ID,
+                false, // tty: false, since the container was not created with TTY
+                new ContainerLogsParameters
+                {
+                    ShowStdout = true,
+                    ShowStderr = true,
+                }
+            );
+
+            var (stdout, stderr) = await logs.ReadOutputToEndAsync(CancellationToken.None);
+
+            // Delete container manually
+            await client.Containers.RemoveContainerAsync(container.ID, new ContainerRemoveParameters());
+
+            string output = stdout + stderr;
+
+            // Cleanup
+            Directory.Delete(tempDir, true);
+
+            return output;
+        }
+    }
+}
