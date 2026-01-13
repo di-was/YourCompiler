e22f95186a2a3412bf31e83edf00062502022e03
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-13 13:53:50 +1100

    DTO for initializing docker container

---
+++ YourCompiler/Infrastructure/ContainerDetails.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace YourCompiler.Infrastructure
+{
+    public record ContainerDetails(
+        string ImageName,
+        string FileName,
+        string LanguageExtension,
+        List<string> ExecutionCommand,
+        string code
+     );
+}
