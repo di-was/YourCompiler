605a7fa1ea03a6aadd528609cb7bf4ebf1223119
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-16 22:30:13 +1100

    Javascript concrete implementation

--- YourCompiler/Application/JavaScriptCompiler.cs
+++ YourCompiler/Application/JavaScriptCompiler.cs
@@ -1,11 +1,21 @@
 ﻿using YourCompiler.Domain;
+using YourCompiler.Infrastructure;
+using YourCompiler.Infrastructure.Interfaces;
 
 namespace YourCompiler.Application
 {
     public class JavaScriptCompiler : ICompiler
     {
+        private readonly IDockerService _dockerService;
+        private readonly IContainerDetailsRegistry _containerDetailsRegistry;
+        public JavaScriptCompiler(IDockerService dockerService, IContainerDetailsRegistry containerDetailsRegistry) { 
+            this._dockerService = dockerService;
+            this._containerDetailsRegistry = containerDetailsRegistry;
+        }
         public CompilerResult Compile(string code) {
-            CompilerResult result = new CompilerResult("Hello World from JavaScript", null);
+            
+            ContainerDetails details = _containerDetailsRegistry.Resolve("javascript") with { Code = code};
+            CompilerResult result = _dockerService.runContainer(details).Result;
             return result;
         }
     }
