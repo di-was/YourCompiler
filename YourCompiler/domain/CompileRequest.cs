9c743ed6e322bc18280fee7d15720017b998db88
Diwas Dahal <diwasdahal2006@gmail.com>
2026-02-16 22:29:01 +1100

    DTO for incoming request

---
+++ YourCompiler/domain/CompileRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace YourCompiler.domain
+{
+    public record CompileRequest
+    {
+        public string Code { get; init; }
+        public string Version { get; init; } = "defaultVersion";
+    }
+}
