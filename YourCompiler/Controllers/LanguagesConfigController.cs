8cce4e4dbad9aa6f2f6af6ab376e54acda203503
Diwas Dahal <diwasdahal2006@gmail.com>
2026-02-18 12:17:22 +1100

    controller for retrieving available languages configuration

---
+++ YourCompiler/Controllers/LanguagesConfigController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YourCompiler.Infrastructure;
+using YourCompiler.Infrastructure.Interfaces;
+
+namespace YourCompiler.Controllers
+{
+    [Route("api/getAvailableLanguages")]
+    [ApiController]
+    public class LanguagesConfigController : ControllerBase
+    {
+        private readonly ILanguageDetailsRegistry _languageDetailsRegistry;
+
+        public LanguagesConfigController(ILanguageDetailsRegistry languageDetailsRegistry)
+        {
+            _languageDetailsRegistry = languageDetailsRegistry;
+        }
+
+        [HttpGet()]
+        public IActionResult getAvailableLanguages()
+        {
+            AvailableLanguagesDTO res = _languageDetailsRegistry.GetAvailableLanguages();
+
+            return Ok(res);
+        }
+    }
+}
