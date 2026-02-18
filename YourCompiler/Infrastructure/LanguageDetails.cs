f92d9d87a7d2fdb843d529fa6950e0771d78b393
Diwas Dahal <diwasdahal2006@gmail.com>
2026-02-18 12:18:58 +1100

    internal DTO for language config

---
+++ YourCompiler/Infrastructure/LanguageDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace YourCompiler.Infrastructure
+{
+
+    public record LanguageConfig(
+        string languageExtension,
+        string[] executionCommand,
+        string defaultVersion,
+        string[] availableVersions,
+        Dictionary<string, string> containerInfo
+        );
+
+    public record LanguagesConfig(
+        Dictionary<string, LanguageConfig> configs,
+        string[] availableLanguages
+        
+        );
+}
