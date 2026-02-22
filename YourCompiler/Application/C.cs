14cdf14a3b6a6242dc9e93f5eab6570c2abfb9ad
Diwas Dahal <diwasdahal2006@gmail.com>
2026-02-22 21:42:07 +1100

    concrete implementation for C compiler

---
+++ YourCompiler/Application/C.cs
@@ -0,0 +1,30 @@
+﻿using YourCompiler.Application.Interfaces;
+using YourCompiler.Domain;
+using YourCompiler.DTOs.InternalDTOs;
+using YourCompiler.Infrastructure.Interfaces;
+
+namespace YourCompiler.Application
+{
+    public class CCompiler : ICompiler
+    {
+        private readonly IDockerService _dockerService;
+        private readonly ILanguageDetailsRegistry _languageDetailsRegistry;
+        private const string languageKey = "c";
+
+        public CCompiler(IDockerService dockerService, ILanguageDetailsRegistry languageDetailsRegistry)
+        {
+            this._dockerService = dockerService;
+            this._languageDetailsRegistry = languageDetailsRegistry;
+        }
+        public CompilerResult Compile(string code, string version)
+        {
+
+            LanguageConfig details = _languageDetailsRegistry.Resolve(languageKey);
+
+            string versionImage = details.containerInfo.GetValueOrDefault(version, details.containerInfo[details.defaultVersion]);
+
+            CompilerResult result = _dockerService.runContainer(details, code, versionImage).Result;
+            return result;
+        }
+    }
+}
